0138b83453e550d5dd071db67a90fa8b55a30833
Egor Shurygin <eshurygin@gmail.com>
2025-08-05 18:03:19 +0300

    Name of commit

---
+++ dev/script.sh
@@ -0,0 +1,11 @@
+token=$1
+name="Шурыгин"
+
+git clone git@github.com:lksh-p/match.git
+git switch task-1
+git switch -c task-1-$token
+grep -n $name CONTRIBUTORS.md | grep -o "[0-9]+"
+#sed 's/Шурыгин/Шурыгин github_login'
+git add *
+git commit -m "Name of commit"
+git push -u origin task-1-$token
