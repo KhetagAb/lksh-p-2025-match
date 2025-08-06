303dfd761afe95848a68178f83b4bace72980a83
Egor Shurygin <shurygin.egor2009@gmail.com>
2025-08-06 11:05:39 +0300

    bot update

--- bff/app/tg_bot/bot.py
+++ bff/app/tg_bot/bot.py
@@ -1,14 +1,12 @@
 from telegram import Bot, Update
-from telegram.ext import Dispatcher
 from fastapi import APIRouter, Request
-import app.config as config
 from telebot.async_telebot import AsyncTeleBot, Handler
 from telebot import types
 import asyncio
 
 router = APIRouter()
 
-real_token = config.settings.telegram_token
+# real_token = config.settings.telegram_token
 token = '8177210409:AAGinnUyakfNtpNsCnafzFEiXGKNaPXA_-U'
 bot = AsyncTeleBot(token)
 
@@ -26,26 +24,26 @@ bot = AsyncTeleBot(token)
 #     return {"ok": True}
 
 
-def get_all_sports():
+def get_list_of_all_sports() -> list[str]:
     return ['Volleyball', 'Basketball', 'Football']
 
 
-def sign_up_to_sport(sport):
+def sign_up_to_sport(sport: str) -> str:
     pass
 
 
-def registration_person(username):
+def registration_person(username: str) -> None:
     pass
 
 
-def get_list_of_sports(sport):
+def get_list_of_people_on_sport(sport: str) -> list[str]:
     pass
 
 
-def make_sports_buttons(pref):
+def make_sports_buttons(pref: str):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = []
-    for sport in get_all_sports():
+    for sport in get_list_of_all_sports():
         buttons.append(types.KeyboardButton(pref + sport))
     markup.add(*buttons)
     return markup
@@ -73,9 +71,14 @@ async def signup(mess):
     await bot.send_message(mess.chat.id, "You can sign up for these sports", reply_markup=make_sports_buttons('reg_'))
 
 
+@bot.message_handler(commands=['list'])
+async def list_(mess):
+    await bot.send_message(mess.chat.id, "To view the lists, select sports", reply_markup=make_sports_buttons('list_'))
+
+
 @bot.message_handler(content_types=['text'])
 async def answer_to_buttons(mess):
-    for sport in get_all_sports():
+    for sport in get_list_of_all_sports():
         if 'reg_' + sport == mess.text:
             ans = sign_up_to_sport(sport)
             msg = ''
@@ -86,13 +89,10 @@ async def answer_to_buttons(mess):
             await bot.send_message(mess.chat.id, msg)
             return
         if 'list_' + sport == mess.text:
-            await bot.send_message(mess.chat.id, get_)
+            await bot.send_message(mess.chat.id, '\n'.join(get_list_of_people_on_sport(sport)))
     await bot.send_message(mess.chat.id, "I don't understand you.")
 
 
-@bot.message_handler(commands=['list'])
-async def list_(mess):
-    await bot.send_message(mess.chat.id, "You can sign up for these sports", reply_markup=make_sports_buttons('list_'))
 
 
 if __name__ == "__main__":
