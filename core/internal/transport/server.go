c90716d7f322bf078a8999f077e6261578a2836b
dmkjfs <dmkjfs@gmail.com>
2025-08-07 15:25:39 +0300

    add pingpong (#26)

--- core/internal/transport/server.go
+++ core/internal/transport/server.go
@@ -8,15 +8,16 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"log/slog"
+
 	"match/internal/handlers"
-	"net/http"
 )
 
 func RegisterEndpoints(server *echo.Echo) {
+	server.GET("/ping", handlers.PingPong)
 	// registering endpoints here
 	// TODO прокинуть сервис игрока
 	server.GET("/", handlers.Handler{}.ValidateRegisterUser)
+
 }
 
 func GetServer() *echo.Echo {
