cf9581aa5a2b3a628c0bda9fceca2ea01315494f
dmkjfs <dmkjfs@gmail.com>
2025-08-08 13:45:46 +0300

    test commit

--- core/internal/transport/server.go
+++ core/internal/transport/server.go
@@ -16,7 +16,7 @@ func RegisterEndpoints(server *echo.Echo) {
 	server.GET("/ping", handlers.PingPong)
 	// registering endpoints here
 	// TODO прокинуть сервис игрока
-	server.GET("/", handlers.Handler{}.ValidateRegisterUser)
+	server.GET("/", handlers.Handler.ValidateRegisterUser)
 }
 
 func GetServer() *echo.Echo {
