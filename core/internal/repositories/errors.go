1a09e6dd90eec7594d2cc750cd50c01ee393ba9d
TBWanderer <125365236+TBWanderer@noreply.users.github.com>
2025-08-08 09:40:35 +0300

    Changed name of error

--- core/internal/repositories/errors.go
+++ core/internal/repositories/errors.go
@@ -13,7 +13,7 @@ func (e *DatabaseError) Error() string {
 	return fmt.Sprintf("DatabaseError code %d: %s", e.Code, e.Message)
 }
 
-var ErrNotFound = DatabaseError{10, "resource not found"}
+var ErrSelecting = DatabaseError{10, "resource not found"}
 var ErrInserting = DatabaseError{11, "failed inserting"}
 var ErrUpdating = DatabaseError{12, "failed updating"}
 var ErrDeleting = DatabaseError{13, "failed deleting"}
