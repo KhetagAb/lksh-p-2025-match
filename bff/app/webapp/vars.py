b086fd491869cd822900c83f46450b80270463da
Misha <mgolovstik@gmail.com>
2025-08-08 15:32:46 +0300

    Improve webapp
    Delete WHITELIST and check_user_in_whitelist function
    Delete print in root
    Move login.html from templates/ to templates/auth

--- bff/app/webapp/vars.py
+++ bff/app/webapp/vars.py
@@ -2,11 +2,7 @@ import hashlib
 import os
 import json
 
-with open('.env', "r", encoding="utf-8") as file:
-    data = json.load(file)
-
 JWT_SECRET_KEY = "hahaha"
-BOT_TOKEN_HASH = hashlib.sha256(data['BOT_TOKEN'].encode())
+BOT_TOKEN_HASH = hashlib.sha256(os.environ["BOT_TOKEN"].encode())
 COOKIE_NAME = 'auth-token'
-WHITELIST = [] #[ "671749585" ]
 ALGORITHM = "HS256"
