36c1c088926444ea561efaeef5997f35bff6cfd4
Maria Shchepareva <m_shchepareva@mail.ru>
2025-08-10 16:28:23 +0300

    add tournament 2

--- bff/lkshmatch/tg_bot/bot.py
+++ bff/lkshmatch/tg_bot/bot.py
@@ -133,7 +133,7 @@ async def start(mess: types.Message) -> None:
     if await fuse_not_nf(mess):
         return
     try:
-        msg = await rest.RestValidateRegisterUser().validate_register_user(rest.PlayerAddInfo(username, int(user_id)))
+        msg = await rest.RestValidateRegisterPlayer().validate_register_user(rest.PlayerAddInfo(username, int(user_id)))
     except rest.PlayerNotFound:
         msg = "Извините, но вас нет в списках. Подойдите в 4-ый комповник."
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -163,7 +163,7 @@ async def answer_to_buttons(mess: types.Message) -> None:
     if mess.text == "Это я, регистрацию подтверждаю.":
         if await fuse_not_nf(mess):
             return
-        response = await rest.RestRegisterUser().register_user(
+        response = await rest.RestRegisterPlayer().register_user(
             rest.PlayerAddInfo(mess.from_user.username, mess.from_user.id))
         add_matching(mess.from_user.id, response)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
