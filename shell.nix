b17693744b384a28bbaab31ca568aaad3afa2249
wrrrzr <wozrer@proton.me>
2025-08-10 15:55:30 +0300

    Add di to webapp

--- shell.nix
+++ shell.nix
@@ -19,6 +19,7 @@ pkgs.mkShell {
         pydantic
         dynaconf
         dishka
+        python-jose
         (pkgs.python3Packages.mkPythonEditablePackage {
           pname = pyproject.project.name;
           version = pyproject.project.version;
