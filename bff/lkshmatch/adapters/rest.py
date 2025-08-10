02a6b876776be6caedaec26f5fda21fab10cdd44
XantMaker <xantmaker@yandex.ru>
2025-08-10 14:59:49 +0300

    Исправленны url в запросах

--- bff/lkshmatch/adapters/rest.py
+++ bff/lkshmatch/adapters/rest.py
@@ -1,4 +1,5 @@
-from bff.lkshmatch.adapters.core import (ValidateRegisterUser, RegisterUser, GetSportSections, GetPlayersBySportSections, Player,
+from bff.lkshmatch.adapters.core import (ValidateRegisterUser, RegisterUser,
+                    GetSportSections, GetPlayersBySportSections,
                    PlayerAddInfo, SportSection, RegisterPlayerInSportSection,
                    PlayerNotFound, UnknownError, PlayerRegisterInfo, Team, NameTeamReserveError,
                     PlayerAlreadyInTeam)
@@ -22,7 +23,7 @@ class RestValidateRegisterUser(ValidateRegisterUser):
     async def validate_register_user(self, user: PlayerAddInfo) -> str:
         async with aiohttp.ClientSession() as session:
             query = {"tg_username": user.tg_username, "tg_id": user.tg_id}
-            response = await session.get(API_URL + '/validate_register_usergit', params=query)
+            response = await session.get(f'{API_URL}/validate_register_user', params=query)
 
             data = await response.json()
 
@@ -38,7 +39,7 @@ class RestRegisterUser(RegisterUser):
     async def register_user(self, user: PlayerAddInfo) -> PlayerRegisterInfo:
         async with aiohttp.ClientSession() as session:
             query = {"tg_username": user.tg_username, "tg_id": user.tg_id}
-            response = await session.get(API_URL + '/register_user', params=query)
+            response = await session.get(f'{API_URL}/register_user', params=query)
 
             if response.status != 200:
                 raise UnknownError
@@ -50,7 +51,7 @@ class RestRegisterUser(RegisterUser):
 class RestGetSportSections(GetSportSections):
     async def get_sections(self) -> list[SportSection]:
         async with aiohttp.ClientSession() as session:
-            response = await session.get(API_URL + '/get_sections')
+            response = await session.get(f'{API_URL}/get_sections')
             if response.status != 200:
                 raise UnknownError
 
@@ -65,7 +66,7 @@ class RestGetPlayersBySportSections(GetPlayersBySportSections):
     async def get_players_by_sport_sections(self, section: SportSection) -> list[PlayerRegisterInfo]:
         async with aiohttp.ClientSession() as session:
             query = {"name_section": section.en_name}
-            response = await session.get(API_URL + '/get_players_by_sport_sections', params=query)
+            response = await session.get(f'{API_URL}/get_players_by_sport_sections', params=query)
             if response.status != 200:
                 raise UnknownError
 
@@ -80,7 +81,7 @@ class RestRegisterPlayerInSportSection(RegisterPlayerInSportSection):
     async def register_player_in_sport_sectoin(self, section: SportSection, user: PlayerRegisterInfo) -> None:
         async with aiohttp.ClientSession() as session:
             query = {"name_section": section.en_name, "user_id": user.id}
-            response = await session.get(API_URL + '/register_player_in_sport_sectoin', params=query)
+            response = await session.get(f'{API_URL}/register_player_in_sport_sectoin', params=query)
             if response.status != 200:
                 raise UnknownError
 
