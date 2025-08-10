cc91c3c7502dbf53a62c8dda1504c831b79fae15
TBWanderer <125365236+TBWanderer@noreply.users.github.com>
2025-08-10 17:29:21 +0300

    Moved to more optimized and pretty way to load resources

--- core/internal/repositories/player.go
+++ core/internal/repositories/player.go
@@ -2,10 +2,25 @@ package repositories
 
 import (
 	"context"
+	_ "embed"
 
 	"match/internal/domain"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var (
+	//go:embed queries/player/create.sql
+	createPlayerQuery string
+
+	//go:embed queries/player/get-by-id.sql
+	getByIDPlayerQuery string
+
+	//go:embed queries/player/get-by-telegram-id.sql
+	getByTelegramIDPlayerQuery string
+
+	//go:embed queries/player/delete.sql
+	deletePlayerQuery string
 )
 
 type Players struct {
@@ -13,13 +28,8 @@ type Players struct {
 }
 
 func (p *Players) CreatePlayer(ctx context.Context, name, username string, telegramID int64) (*int64, error) {
-	query, err := GetQuery("player", "create")
-	if err != nil {
-		return nil, &domain.FailedLoadingResourcesError{Code: domain.FailedLoadingResources, Message: err.Error()}
-	}
-
 	var id int64
-	err = p.pool.QueryRow(ctx, *query, name, username, telegramID).Scan(&id)
+	err := p.pool.QueryRow(ctx, createPlayerQuery, name, username, telegramID).Scan(&id)
 	if err != nil {
 		return nil, &domain.InvalidOperationError{Code: domain.InvalidOperation, Message: err.Error()}
 	}
@@ -28,16 +38,11 @@ func (p *Players) CreatePlayer(ctx context.Context, name, username string, teleg
 }
 
 func (p *Players) GetPlayerByID(ctx context.Context, id int64) (*Player, error) {
-	query, err := GetQuery("player", "get-by-id")
-	if err != nil {
-		return nil, &domain.FailedLoadingResourcesError{Code: domain.FailedLoadingResources, Message: err.Error()}
-	}
-
 	var telegramID int64
 	var name string
 	var username string
 
-	err = p.pool.QueryRow(ctx, *query, id).Scan(&id, &name, &username, &telegramID)
+	err := p.pool.QueryRow(ctx, getByIDPlayerQuery, id).Scan(&id, &name, &username, &telegramID)
 	if err != nil {
 		return nil, &domain.NotFoundError{Code: domain.NotFound, Message: err.Error()}
 	}
@@ -46,16 +51,11 @@ func (p *Players) GetPlayerByID(ctx context.Context, id int64) (*Player, error) 
 }
 
 func (p *Players) GetPlayerByTelegramID(ctx context.Context, telegramID int64) (*Player, error) {
-	query, err := GetQuery("player", "get-by-telegram-id")
-	if err != nil {
-		return nil, &domain.FailedLoadingResourcesError{Code: domain.FailedLoadingResources, Message: err.Error()}
-	}
-
 	var id int64
 	var name string
 	var username string
 
-	err = p.pool.QueryRow(ctx, *query, telegramID).Scan(&id, &name, &username, &telegramID)
+	err := p.pool.QueryRow(ctx, getByTelegramIDPlayerQuery, telegramID).Scan(&id, &name, &username, &telegramID)
 	if err != nil {
 		return nil, &domain.NotFoundError{Code: domain.NotFound, Message: err.Error()}
 	}
@@ -64,14 +64,12 @@ func (p *Players) GetPlayerByTelegramID(ctx context.Context, telegramID int64) (
 }
 
 func (p *Players) DeletePlayerByID(ctx context.Context, id int64) error {
-	query, err := GetQuery("player", "delete")
-	if err != nil {
-		return &domain.FailedLoadingResourcesError{Code: domain.FailedLoadingResources, Message: err.Error()}
-	}
-
-	_, err = p.pool.Exec(ctx, *query, id)
+	commandTag, err := p.pool.Exec(ctx, deletePlayerQuery, id)
 	if err != nil {
 		return &domain.InvalidOperationError{Code: domain.InvalidOperation, Message: err.Error()}
+	}
+	if commandTag.RowsAffected() != 1 {
+		return &domain.NotFoundError{Code: domain.NotFound, Message: err.Error()}
 	}
 
 	return nil
