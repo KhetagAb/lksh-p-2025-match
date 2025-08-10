1aa107d7174e869a98397c3bfd7d4453307b4690
masha <156920178+mashashepp@users.noreply.github.com>
2025-08-10 16:41:30 +0300

    add tournament (#38)
    * Добавление ручки регистрации человека в спорт секции
    
    * add tournament 2

---
+++ bff/lkshmatch/adapters/players.py
@@ -0,0 +1,34 @@
+import aiohttp
+
+from bff.lkshmatch.adapters.core import ValidateRegisterPlayer, PlayerAddInfo, UnknownError, RegisterPlayer, \
+    PlayerRegisterInfo, API_URL
+from bff.lkshmatch.adapters.sport_sections import PlayerNotFoundResponse
+
+
+class RestValidateRegisterPlayer(ValidateRegisterPlayer):
+    async def validate_register_user(self, user: PlayerAddInfo) -> str:
+        async with aiohttp.ClientSession() as session:
+            query = {"tg_username": user.tg_username, "tg_id": user.tg_id}
+            response = await session.get(f'{API_URL}/validate_register_user', params=query)
+
+            data = await response.json()
+
+            if response.status == 404 and data.count("detail") != 0:
+                raise PlayerNotFoundResponse
+            if response.status != 200:
+                raise UnknownError
+
+            return data["name"]
+
+
+class RestRegisterPlayer(RegisterPlayer):
+    async def register_user(self, user: PlayerAddInfo) -> PlayerRegisterInfo:
+        async with aiohttp.ClientSession() as session:
+            query = {"tg_username": user.tg_username, "tg_id": user.tg_id}
+            response = await session.get(f'{API_URL}/register_user', params=query)
+
+            if response.status != 200:
+                raise UnknownError
+
+            data = await response.json()
+            return PlayerRegisterInfo(data["name"], int(data['id']))
