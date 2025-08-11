e8eaa698bfcc0d2ea6d65ebd04a47214db4aa57d
TimothyOdintsov <tim@timodi.ru>
2025-08-11 15:43:08 +0300

    tim added sport list (#56)

---
+++ core/internal/services/sport.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"context"
+	"match/internal/repositories"
+)
+
+type (
+	GetAllSportSectionService interface {
+		//TODO возвращать доменный объект
+		GetSportsList(ctx context.Context) ([]*repositories.Sport, error)
+	}
+
+	SportService struct {
+		repository GetAllSportSectionService
+	}
+)
+
+func (s *SportService) GetAllSportSection(ctx context.Context) ([]string, error) {
+	cnt, err := s.repository.GetSportsList(ctx)
+	if cnt == nil || err != nil {
+		return nil, err
+	} else {
+		var ans []string
+		for _, el := range cnt {
+			ans = append(ans, el.Title)
+
+		}
+		return ans, nil
+
+	}
+}
+
