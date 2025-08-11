b67c0f83c3ec9af77ec59b9f08f9b8626957bd2c
TBWanderer <125365236+TBWanderer@noreply.users.github.com>
2025-08-11 12:05:32 +0300

    Refactor players methods to actual sheme

--- core/internal/repositories/player.go
+++ core/internal/repositories/player.go
@@ -15,8 +15,11 @@ var (
 	//go:embed queries/player/get-by-id.sql
 	getByIDPlayerQuery string
 
-	//go:embed queries/player/get-by-telegram-id.sql
-	getByTelegramIDPlayerQuery string
+	//go:embed queries/player/get-by-tg-id.sql
+	getByTgIDPlayerQuery string
+
+	//go:embed queries/player/get-by-tg-username.sql
+	getByTgUsernamePlayerQuery string
 
 	//go:embed queries/player/delete.sql
 	deletePlayerQuery string
@@ -32,9 +35,9 @@ func NewPlayersRepository(
 	return &Players{pool: pool}
 }
 
-func (p *Players) CreatePlayer(ctx context.Context, name, username string, telegramID int) (*int, error) {
-	var id int
-	err := p.pool.QueryRow(ctx, createPlayerQuery, name, username, telegramID).Scan(&id)
+func (p *Players) CreatePlayer(ctx context.Context, name, username string, tgID int64) (*int64, error) {
+	var id int64
+	err := p.pool.QueryRow(ctx, createPlayerQuery, name, username, tgID).Scan(&id)
 	if err != nil {
 		return nil, &domain.InvalidOperationError{Code: domain.InvalidOperation, Message: err.Error()}
 	}
@@ -42,33 +45,46 @@ func (p *Players) CreatePlayer(ctx context.Context, name, username string, teleg
 	return &id, nil
 }
 
-func (p *Players) GetPlayerByID(ctx context.Context, id int) (*domain.Player, error) {
-	var telegramID int
+func (p *Players) GetPlayerByID(ctx context.Context, id int64) (*domain.Player, error) {
+	var tgID int64
 	var name string
 	var username string
 
-	err := p.pool.QueryRow(ctx, getByIDPlayerQuery, id).Scan(&id, &name, &username, &telegramID)
+	err := p.pool.QueryRow(ctx, getByIDPlayerQuery, id).Scan(&id, &name, &username, &tgID)
 	if err != nil {
 		return nil, &domain.NotFoundError{Code: domain.NotFound, Message: err.Error()}
 	}
 
-	return &domain.Player{ID: id, Name: name, Username: username, TelegramID: telegramID}, nil
+	return &domain.Player{ID: id, Name: name, TgUsername: username, TgID: tgID}, nil
 }
 
-func (p *Players) GetPlayerByTelegramID(ctx context.Context, telegramID int) (*domain.Player, error) {
-	var id int
+func (p *Players) GetPlayerByTgID(ctx context.Context, tgID int64) (*domain.Player, error) {
+	var id int64
 	var name string
 	var username string
 
-	err := p.pool.QueryRow(ctx, getByTelegramIDPlayerQuery, telegramID).Scan(&id, &name, &username, &telegramID)
+	err := p.pool.QueryRow(ctx, getByTgIDPlayerQuery, tgID).Scan(&id, &name, &username, &tgID)
 	if err != nil {
 		return nil, &domain.NotFoundError{Code: domain.NotFound, Message: err.Error()}
 	}
 
-	return &domain.Player{ID: id, Name: name, Username: username, TelegramID: telegramID}, nil
+	return &domain.Player{ID: id, Name: name, TgUsername: username, TgID: tgID}, nil
 }
 
-func (p *Players) DeletePlayerByID(ctx context.Context, id int) error {
+func (p *Players) GetPlayerByTgUsername(ctx context.Context, username string) (*domain.Player, error) {
+	var id int64
+	var name string
+	var tgID int64
+
+	err := p.pool.QueryRow(ctx, getByTgUsernamePlayerQuery, username).Scan(&id, &name, &username, &tgID)
+	if err != nil {
+		return nil, &domain.NotFoundError{Code: domain.NotFound, Message: err.Error()}
+	}
+
+	return &domain.Player{ID: id, Name: name, TgUsername: username, TgID: tgID}, nil
+}
+
+func (p *Players) DeletePlayerByID(ctx context.Context, id int64) error {
 	commandTag, err := p.pool.Exec(ctx, deletePlayerQuery, id)
 	if err != nil {
 		return &domain.InvalidOperationError{Code: domain.InvalidOperation, Message: err.Error()}
