90178e42d792b615c0bf75fd45c3e5f4573b8e9e
XantMaker <xantmaker@yandex.ru>
2025-08-11 15:47:48 +0300

    Добавлние монго и операций для команд

--- bff/lkshmatch/adapters/players.py
+++ bff/lkshmatch/adapters/players.py
@@ -1,24 +1,30 @@
 import aiohttp
 
-from bff.lkshmatch.adapters.core import ValidateRegisterPlayer, PlayerAddInfo, UnknownError, RegisterPlayer, \
-    PlayerRegisterInfo, API_URL
+from bff.lkshmatch.adapters.core import (ValidateRegisterPlayer, PlayerAddInfo, UnknownError, RegisterPlayer,
+    PlayerRegisterInfo, PlayerAlreadyRegister, GetPlayerId, API_URL)
 from bff.lkshmatch.adapters.sport_sections import PlayerNotFoundResponse
+from ... import MongoValidateRegisterPlayer
 
+class RestGetPlayerId(GetPlayerId):
+    async def get_player_id(self, user: PlayerAddInfo) -> int:
+        async with aiohttp.ClientSession() as session:
+            query = {"tg_username": user.tg_username, "tg_id": user.tg_id}
+            response = await session.get(f'{API_URL}/register_user', params=query)
+
+            if response.status != 200:
+                raise UnknownError
+
+            data = await response.json()
+            return int(data['id'])
 
 class RestValidateRegisterPlayer(ValidateRegisterPlayer):
     async def validate_register_user(self, user: PlayerAddInfo) -> str:
         async with aiohttp.ClientSession() as session:
-            query = {"tg_username": user.tg_username, "tg_id": user.tg_id}
-            response = await session.get(f'{API_URL}/validate_register_user', params=query)
-
-            data = await response.json()
-
-            if response.status == 404 and data.count("detail") != 0:
+            name, flag = MongoValidateRegisterPlayer(user)
+            if flag == False:
                 raise PlayerNotFoundResponse
-            if response.status != 200:
-                raise UnknownError
 
-            return data["name"]
+            return name
 
 
 class RestRegisterPlayer(RegisterPlayer):
@@ -27,6 +33,8 @@ class RestRegisterPlayer(RegisterPlayer):
             query = {"tg_username": user.tg_username, "tg_id": user.tg_id}
             response = await session.get(f'{API_URL}/register_user', params=query)
 
+            if response.status == 409:
+                raise PlayerAlreadyRegister
             if response.status != 200:
                 raise UnknownError
 
