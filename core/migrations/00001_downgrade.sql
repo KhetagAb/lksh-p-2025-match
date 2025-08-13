1e7751c093f7f5d4847a774587b8cfc1a27a4dd8
dmkjfs <dmkjfs@gmail.com>
2025-08-13 16:40:04 +0300

    lil fix

--- core/migrations/00001_downgrade.sql
+++ core/migrations/00001_downgrade.sql
@@ -1,9 +1,9 @@
 -- 2025-08-13 ---
 
-create table players;
-create table teams;
-create table team_players;
-create table sport_sections;
-create table activities;
-create table meetings;
-create table meeting_participants;
+drop table players;
+drop table teams;
+drop table team_players;
+drop table sport_sections;
+drop table activities;
+drop table meetings;
+drop table meeting_participants;
