1f88cb829925c476aeca11d610f15249461b8d61
masha <156920178+mashashepp@users.noreply.github.com>
2025-08-13 15:43:36 +0300

    add openapi (#87)

--- bff/lkshmatch/adapters/core/sport_sections.py
+++ bff/lkshmatch/adapters/core/sport_sections.py
@@ -1,32 +1,49 @@
+import os
 from typing import List
 
 import aiohttp
+from pymongo import MongoClient
 
-from lkshmatch.adapters.base import CorePlayer, SportAdapter, SportSection
+import core_client
+from core_client.api.default import get_core_sport_get_sport, get_core_sports_get, get_core_sports_get_all
+from core_client.types import UNSET, Unset
+from lkshmatch.adapters.base import CorePlayer, SportAdapter, SportSection, UnknownError, API_URL
+from lkshmatch.config import settings
+from lkshmatch.repositories.mongo.students import MongoLKSHStudentsRepository
+
+
+class PlayerRegisterInfo:
+    pass
 
 
 class CoreSportAdapter(SportAdapter):
-    async def get_sections(self) -> List[SportSection]:
-        async with aiohttp.ClientSession() as session:
-            response = await session.get(f"{API_URL}/get_sections")
-            if response.status != 200:
-                raise UnknownError
+    def __init__(self):
+        # TODO DI
+        core_client_url = f"{settings.get('CORE_HOST')}:{settings.get('CORE_PORT')}"
+        self.client = core_client.Client(base_url=core_client_url)
 
-            data = await response.json()
-            return [SportSection(name, en_name) for name, en_name in data]
+    async def get_sections(self) -> List[SportSection]:
+        response = await get_core_sports_get.asyncio(
+            client=self.client
+        )
+        #TODO че то там
+        #if (response.sports_sections != Unset):
+        #   return response.sports_sections
 
     async def get_players_by_sport_sections(self, section: SportSection) -> List[CorePlayer]:
-        async with aiohttp.ClientSession() as session:
-            query = {"name_section": section.en_name}
-            response = await session.get(f"{API_URL}/get_players_by_sport_sections", params=query)
-            if response.status != 200:
-                raise UnknownError
+        response = await get_core_sport_get_sport.asyncio(
+            client=self.client, sport=section.sport
+        )
 
-            data = await response.json()
-            return [PlayerRegisterInfo(name, id_player) for name, id_player in data]
+    async def get_all_sections(self) -> List[SportSection[List[CorePlayer]]]:
+        response = await get_core_sports_get_all.asyncio(
+            client=self.client
+        )
+
+
+
 
     async def register_player_in_sport_section(self, section: SportSection, user: CorePlayer) -> None:
-        # проверка на админа
         async with aiohttp.ClientSession() as session:
             query = {"name_section": section.en_name, "user_id": user.id}
             response = await session.get(f"{API_URL}/register_player_in_sport_section", params=query)
--- bff/lkshmatch/repositories/mongo/students.py
+++ bff/lkshmatch/repositories/mongo/students.py
@@ -16,6 +16,6 @@ class MongoLKSHStudentsRepository(LKSHStudentsRepository):
     async def validate_register_user(self, user: Player) -> str:
         students = await self.get_students()
         for student in students:
-            if student['tg_username'] == user.tg_username:
-                return student['name']
+            if student["tg_username"] == user.tg_username:
+                return student["name"]
         raise PlayerNotFound
