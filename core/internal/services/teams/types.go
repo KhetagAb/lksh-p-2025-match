f6753cf48dde85b4490d636a04b6e5a9bbd2aebb
TBWanderer <125365236+TBWanderer@users.noreply.github.com>
2025-08-14 10:48:24 +0300

    Added teams service

---
+++ core/internal/services/teams/types.go
@@ -0,0 +1,32 @@
+package teams
+
+import (
+	"context"
+	"match/domain"
+)
+
+type (
+	TeamRepository interface {
+		GetTeamsByActivityID(ctx context.Context, activityID int64) ([]domain.Team, error)
+		GetTeamPlayersByID(ctx context.Context, teamID int64) ([]domain.Player, error)
+	}
+
+	PlayerRepository interface {
+		GetPlayerByID(ctx context.Context, id int64) (*domain.Player, error)
+	}
+
+	TeamService struct {
+		teamRepository   TeamRepository
+		playerRepository PlayerRepository
+	}
+)
+
+func NewTeamService(
+	teamRepository TeamRepository,
+	playerRepository PlayerRepository,
+) *TeamService {
+	return &TeamService{
+		teamRepository:   teamRepository,
+		playerRepository: playerRepository,
+	}
+}
