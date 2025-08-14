95202386ba0abbf2990e96c349d32aa0d6b93598
Maria Shchepareva <m_shchepareva@mail.ru>
2025-08-14 13:34:42 +0300

    FIX add mappers

--- bff/lkshmatch/adapters/core/mappers/activity.py
+++ bff/lkshmatch/adapters/core/mappers/activity.py
@@ -1,14 +1,14 @@
 from lkshmatch.adapters.base import CorePlayer, Activity, Team
 
 
-def map_core_player(activity) -> CorePlayer:
+def map_core_player(activity:Activity) -> CorePlayer:
     return CorePlayer(
         core_id=activity.creator.core_id,
         tg_id=activity.creator.tg_id,
     )
 
 
-def map_activity(activity) -> Activity:
+def map_activity(activity:Activity) -> Activity:
     return Activity(
         id=activity.id,
         title=activity.title,
@@ -16,7 +16,7 @@ def map_activity(activity) -> Activity:
         creator=map_core_player(activity)
     )
 
-def map_team(team) -> Team:
+def map_team(team:Team) -> Team:
     return Team(
         id=team.id,
         name=team.name,
--- bff/lkshmatch/adapters/core/mappers/sport_section.py
+++ bff/lkshmatch/adapters/core/mappers/sport_section.py
@@ -1,5 +1,5 @@
 from core_client.models import SportSection
 
 
-def map_sport_section(sport)->SportSection:
+def map_sport_section(sport:SportSection)->SportSection:
     return SportSection(id=sport.id, name=sport.name, ru_name=sport.ru_name)
