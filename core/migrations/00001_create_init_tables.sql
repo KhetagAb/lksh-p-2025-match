3eeb611cf48365d3e4070e7a44b01b12cb3263f3
dmkjfs <dmkjfs@gmail.com>
2025-08-14 11:03:16 +0300

    link migrations (#89)
    * add migrations
    
    * lil fix
    
    * Update .gitignore
    
    * fix
    
    * Update .env.example
    
    * Update Makefile
    
    * Update dev.dockerfile
    
    * Update prod.dockerfile

--- core/migrations/00001_create_init_tables.sql
+++ core/migrations/00001_create_init_tables.sql
@@ -7,7 +7,7 @@ create table players
     name        varchar(64) not null,
     tg_username varchar(32) not null,
     tg_id       bigint      not null,
-	constraint players_tg_id_unique unique (tg_id)
+    constraint players_tg_id_unique unique (tg_id)
 );
 
 create table teams
@@ -20,8 +20,8 @@ create table teams
 
 create table team_players
 (
-	player_id bigint primary key not null references players (id),
-	team_id   bigint primary key not null references teams (id)
+    player_id bigint primary key not null references players (id),
+    team_id   bigint primary key not null references teams (id)
 );
 
 create table sport_sections
@@ -35,25 +35,25 @@ create table activities
 (
     id               bigserial primary key,
     title            varchar(64) not null,
-	description      text        not null,
+    description      text        not null,
     sport_section_id bigint      not null references sport_sections (id),
-	creator_id       bigint      not null references players (id)
+    creator_id       bigint      not null references players (id)
 );
 
 create table meetings
 (
-	id           bigserial primary key,
-	activity_id  bigint                   not null references activities (id),
-	title        varchar(64)              not null,
-	date         timestamp with time zone not null,
-	details      text                     not null,
-	status       varchar(32)              not null
+    id           bigserial primary key,
+    activity_id  bigint                   not null references activities (id),
+    title        varchar(64)              not null,
+    date         timestamp with time zone not null,
+    details      text                     not null,
+    status       varchar(32)              not null
 );
 
 create table meeting_participants
 (
-	player_id  bigint not null references players (id),
-	meeting_id bigint not null references meetings (id)
+    player_id  bigint not null references players (id),
+    meeting_id bigint not null references meetings (id)
 );
 
 -- +goose StatementEnd
