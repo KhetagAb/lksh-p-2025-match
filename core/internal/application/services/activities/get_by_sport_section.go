3d9dff9c44411bc198ab3c7f8743c29465cdedd0
TBWanderer <125365236+TBWanderer@users.noreply.github.com>
2025-08-15 11:48:12 +0300

    Moved to dto models #3 [issue#104]

--- core/internal/application/services/activities/get_by_sport_section.go
+++ core/internal/application/services/activities/get_by_sport_section.go
@@ -3,24 +3,27 @@ package activities
 import (
 	"context"
 	"fmt"
-	"match/internal/domain/dao"
+	"match/internal/domain/dto"
 )
 
-func (s *ActivityService) GetActivitiesBySportSectionID(ctx context.Context, sportSectionID int64) ([]dao.Activity, []dao.Player, error) {
-	activityList, err := s.activityRepository.GetActivitiesBySportSectionID(ctx, sportSectionID)
+func (s *ActivityService) GetActivitiesBySportSectionID(ctx context.Context, sportSectionID int64) (dto.Activities, error) {
+	activities, err := s.activityRepository.GetActivitiesBySportSectionID(ctx, sportSectionID)
 	if err != nil {
-		return nil, nil, fmt.Errorf("cannot get list activities by sport section id [sport_section_id=%d]: %w", sportSectionID, err)
+		return nil, fmt.Errorf("cannot get list activities by sport section id [sport_section_id=%d]: %w", sportSectionID, err)
 	}
 
-	var creatorList []dao.Player
+	var activitiesDTO dto.Activities
 
-	for _, activity := range activityList {
+	for _, activity := range activities {
 		creator, err := s.playerRepository.GetPlayerByID(ctx, activity.CreatorID)
 		if err != nil {
-			return nil, nil, fmt.Errorf("cannot get creator of activity by id [activity_id=%d] [activity_title=%s] [activity_creator_id=%d]: %w", activity.ID, activity.Title, activity.CreatorID, err)
+			return nil, fmt.Errorf("cannot get creator of activity by id [activity_id=%d] [activity_title=%s] [activity_creator_id=%d]: %w", activity.ID, activity.Title, activity.CreatorID, err)
 		}
-		creatorList = append(creatorList, *creator)
+
+		activityDTO := dto.Activity{Activity: activity, Creator: *creator}
+
+		activitiesDTO = append(activitiesDTO, activityDTO)
 	}
 
-	return activityList, creatorList, nil
+	return activitiesDTO, nil
 }
