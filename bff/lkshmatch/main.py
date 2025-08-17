99c334ae04c9eb4707ca750c56a806165f797afd
dmkjfs <dmkjfs@gmail.com>
2025-08-17 10:47:55 +0300

    add multi-threading

--- bff/lkshmatch/main.py
+++ bff/lkshmatch/main.py
@@ -1,17 +1,38 @@
 import asyncio
+import uvloop
 
+import uvicorn
 from fastapi import FastAPI
 
-from lkshmatch.config import settings
-from lkshmatch.tg_bot.bot import bot as telegram_bot
+from lkshmatch.tg_bot.bot import bot
 
 
-def print_loaded_settings():
-    print("Loaded settings:")
-    for key, value in settings.as_dict().items():
-        print(f"{key} = {value!r}")
+async def start_app() -> None:
+    app = FastAPI(
+        title="Match REST API",
+        summary="REST API documentation for Match",
+        version="0.1.0",
+        redoc_url="/",
+    )
+    uvicorn.run(
+        app=app,
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        reload_dirs=["."],
+        log_config="../logging.ini",
+        log_level="DEBUG",
+        env_file="../.env",
+    )
 
 
-app = FastAPI()
-print_loaded_settings()
-asyncio.run(telegram_bot.polling(non_stop=True))
+async def start() -> ...:
+    uvloop.install()
+    loop = asyncio.get_event_loop()
+
+    await loop.run_in_executor(None, bot.polling(none_stop=True), 1)
+    await start_app()
+
+
+if __name__ == "__main__":
+    asyncio.run(start())
