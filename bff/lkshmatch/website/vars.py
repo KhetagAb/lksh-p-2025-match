e2532a5cd460d97c35633f065ccb98d205ec47cd
Misha <mgolovstik@gmail.com>
2025-08-17 14:44:03 +0300

    lil fix

--- bff/lkshmatch/website/vars.py
+++ bff/lkshmatch/website/vars.py
@@ -1,12 +1,15 @@
 import hashlib
 import os
+from lkshmatch.config import settings
 
 # Telegram auth
-JWT_SECRET_KEY = os.getenv("WEBAPP_JWT_SECRET_KEY")
-BOT_TOKEN_HASH = hashlib.sha256(os.getenv("TELEGRAM_TOKEN").encode())
+JWT_SECRET_KEY = settings.get("WEBSITE_JWT_SECRET_KEY")
+BOT_TOKEN_HASH = hashlib.sha256(settings.get("TELEGRAM_TOKEN").encode())
 COOKIE_NAME = "auth-token"
 ALGORITHM = "HS256"
 
 # Google sheets
-CREDENTIALS_FILE = os.getenv("WEBAPP_CREDENTIALS_FILE")  # имя файла с закрытым ключом для google-таблиц
-SERVICE_ACCOUNT_NAME = os.getenv("WEBAPP_SERVICE_ACCOUNT_NAME")  # почта сервисного аккаунта
+CREDENTIALS_FILE = settings.get("WEBSITE_CREDENTIALS_FILE")  # имя файла с закрытым ключом для google-таблиц
+SERVICE_ACCOUNT_NAME = settings.get("WEBSITE_SERVICE_ACCOUNT_NAME")  # почта сервисного аккаунта
+WEBSITE_IP = settings.get("WEBSITE_IP")
+WEBSITE_PORT = settings.get("WEBSITE_PORT")
