7c7ba365987780d513df602f5057f100110d010b
Misha <mgolovstik@gmail.com>
2025-08-18 14:13:27 +0300

    fix

--- bff/lkshmatch/website/main.py
+++ bff/lkshmatch/website/main.py
@@ -1,11 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
-from lkshmatch.di import WEBSITE_IP, WEBSITE_PORT
 from lkshmatch.website.auth.auth import auth_router
 from lkshmatch.website.auth.login_middleware import LoginWallMiddleware
 from lkshmatch.website.activities import activities_router
-from lkshmatch.website.gsheets import table_adapter_router
+from lkshmatch.website.activities_gsheets import table_adapter_router
+
+from lkshmatch.config import settings
 
 app = FastAPI()
 
@@ -14,6 +15,9 @@ app.include_router(table_adapter_router, prefix="/table")
 app.include_router(activities_router, prefix="")
 
 app.add_middleware(LoginWallMiddleware)
+
+WEBSITE_IP = settings.get("WEBSITE_IP")
+WEBSITE_PORT = settings.get("WEBSITE_PORT")
 
 if __name__ == "__main__":
     print("RUN WEBAPP")
