08f2bd7e5225e4207d6a03d7afb53b10ff63ab2c
Vadim143 <kokorev.vadim.123456789@gmail.com>
2025-08-18 13:44:01 +0300

    made create_activity handle

--- core/internal/application/repositories/activities.go
+++ core/internal/application/repositories/activities.go
@@ -15,12 +15,25 @@ var getActivitiesBySportSectionIDQuery string
 //go:embed queries/activity/get-by-id.sql
 var getActivityByIDQuery string
 
+//go:embed queries/activity/create-activity.sql
+var createActivity string
+
 type Activities struct {
 	pool *pgxpool.Pool
 }
 
 func NewActivitiesRepository(pool *pgxpool.Pool) *Activities {
 	return &Activities{pool: pool}
+}
+
+func (a *Activities) CreateActivity(ctx context.Context, creatorID, sportSectionId int64, title, description string) (*dao.Activity, error) {
+	var activity dao.Activity
+	err := a.pool.QueryRow(ctx, createActivity, title, description, sportSectionId, creatorID).
+		Scan(activity.ID, activity.Title, activity.Description, activity.SportSectionID, activity.CreatorID)
+	if err != nil {
+		return nil, &services.InvalidOperationError{Code: services.InvalidOperation, Message: err.Error()}
+	}
+	return &activity, nil
 }
 
 func (a *Activities) GetActivitiesBySportSectionID(ctx context.Context, sportSectionID int64) ([]dao.Activity, error) {
@@ -49,7 +62,8 @@ func (a *Activities) GetActivitiesBySportSectionID(ctx context.Context, sportSec
 
 func (a *Activities) GetActivityByID(ctx context.Context, id int64) (*dao.Activity, error) {
 	var activity dao.Activity
-	err := a.pool.QueryRow(ctx, getActivityByIDQuery, id).Scan(&activity.ID, &activity.Title, &activity.Description, &activity.SportSectionID, &activity.CreatorID)
+	err := a.pool.QueryRow(ctx, getActivityByIDQuery, id).
+		Scan(&activity.ID, &activity.Title, &activity.Description, &activity.SportSectionID, &activity.CreatorID)
 	if err != nil {
 		return nil, &services.NotFoundError{Code: services.NotFound, Message: err.Error()}
 	}
