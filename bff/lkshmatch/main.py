1189c842b8b9ca77e69a6ee6de9cc533aa4184a8
dmkjfs <dmkjfs@gmail.com>
2025-08-18 14:45:32 +0300

    fix

--- bff/lkshmatch/main.py
+++ bff/lkshmatch/main.py
@@ -5,10 +5,9 @@ import uvloop
 from fastapi import FastAPI
 
 from lkshmatch.tg_bot.bot import token, bot, router as bot_router
-from lkshmatch.webapp.auth import auth_router
-from lkshmatch.webapp.login_wall import LoginWallMiddleware
-from lkshmatch.webapp.root import root_router
-from lkshmatch.webapp.table_adapter import table_adapter_router
+from lkshmatch.website.auth.auth import auth_router
+from lkshmatch.website.auth.login_middleware import LoginWallMiddleware
+from lkshmatch.website.activities_gsheets import table_adapter_router
 from lkshmatch.config import settings
 
 
@@ -31,7 +30,6 @@ async def start() -> None:
     )
     app.include_router(auth_router, prefix="/auth")
     app.include_router(table_adapter_router, prefix="/table")
-    app.include_router(root_router, prefix="")
     app.include_router(bot_router)
 
     app.add_middleware(LoginWallMiddleware)
