49bb5a9c6efb80f05af3f114be54c5cc1c31d7cf
Khetag Dzestelov <dzkhetag@gmail.com>
2025-08-19 09:16:44 +0300

    Experimental check.yml

--- core/migrations/upgrade.sh
+++ core/migrations/upgrade.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # only call from core
-go tool github.com/pressly/goose/v3/cmd/goose \
+$(go env GOPATH)/bin/goose \
   -dir migrations \
   postgres "host=${POSTGRES_HOST} port=${POSTGRES_PORT} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD} dbname=${POSTGRES_DB} sslmode=disable" \
   up
--- core/tools.go
+++ core/tools.go
@@ -6,4 +6,5 @@ package main
 import (
 	_ "github.com/google/wire/cmd/wire"
 	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
+	_ "github.com/pressly/goose/v3/cmd/goose"
 )
