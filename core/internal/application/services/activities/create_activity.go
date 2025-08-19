995143edfbd4ec6c333dc1233a44c90d7e12a0ae
Vadim143 <kokorev.vadim.123456789@gmail.com>
2025-08-19 10:10:58 +0300

    fixing create_activity handle

--- core/internal/application/services/activities/create_activity.go
+++ core/internal/application/services/activities/create_activity.go
@@ -18,6 +18,25 @@ func (s *ActivityService) CreateActivity(ctx context.Context, creatorID, sportSe
 		return nil, fmt.Errorf("cannot get player by tg_id [player_tg_id=%d]: %w", creatorID, err)
 	}
 
+	if err != nil {
+		infra.Errorf(ctx, "Creator doesn't exist: %v", creatorID)
+		return nil, fmt.Errorf("cannot get player by tg_id [player_tg_id=%d]: %w", creatorID, err)
+	}
+
+	// Checking sports section existence
+	infra.Infof(ctx, "Checking if sports section exists: %v", sportSectionId)
+	sportSections, err := s.sportRepository.GetAllSportSection(ctx)
+	flag := false
+	for _, sportSection := range sportSections {
+		if sportSection.ID == sportSectionId {
+			flag = true
+		}
+	}
+	if !flag {
+		infra.Errorf(ctx, "Sport section doesn't exist: %v", sportSectionId)
+		return nil, fmt.Errorf("cannot get sport section by id [sport_section_id=%d]: %w", sportSectionId, err)
+	}
+
 	// Creating activity
 	infra.Infof(ctx, "Creating activity: [creatorId=%d][sportSectionId=%d][title=%s][description=%s]", creatorID, sportSectionId, title, description)
 	activity, err := s.activityRepository.CreateActivity(ctx, creatorID, sportSectionId, title, description)
