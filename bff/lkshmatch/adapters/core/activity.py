e53a49407c9900ef664b7ea404b05e5618e1f717
Maria Shchepareva <m_shchepareva@mail.ru>
2025-08-20 09:49:19 +0300

    add ручку leave player by activity

--- bff/lkshmatch/adapters/core/activity.py
+++ bff/lkshmatch/adapters/core/activity.py
@@ -11,14 +11,14 @@ from lkshmatch.adapters.core.mappers.activity import map_team, map_activity
 from lkshmatch.adapters.core.mappers.player import map_player_from_api
 from lkshmatch.core_client.api.activities import get_core_teams_by_activity_id, post_core_activity_id_enroll, \
     post_core_activity_create, post_core_activity_delete_by_id, post_core_activity_update_by_id, \
-    get_core_activities_by_sport_section_id
+    get_core_activities_by_sport_section_id, post_core_activity_id_leave
 from lkshmatch.core_client.models import GetCoreActivitiesBySportSectionIdResponse400, \
     GetCoreActivitiesBySportSectionIdResponse200, GetCoreTeamsByActivityIdResponse400, \
     GetCoreTeamsByActivityIdResponse200, PostCoreActivityIdEnrollResponse400, PostCoreActivityIdEnrollResponse409, \
     PostCoreActivityIdEnrollResponse200, ActivityEnrollPlayerRequest, CreateActivityRequest, \
     PostCoreActivityCreateResponse400, PostCoreActivityCreateResponse200, PostCoreActivityDeleteByIdResponse400, \
     PostCoreActivityDeleteByIdResponse200, PostCoreActivityUpdateByIdResponse200, PostCoreActivityUpdateByIdResponse400, \
-    UpdateActivityRequest
+    UpdateActivityRequest, ActivityLeavePlayerRequest, PostCoreActivityIdLeaveResponse404
 from lkshmatch.core_client.types import Unset, UNSET
 
 
@@ -64,9 +64,14 @@ class CoreActivityAdapter(ActivityAdapter):
             raise PlayerAlreadyInTeam(f"Player is already enrolled in a team for this activity: {response.message}")
         if not isinstance(response, PostCoreActivityIdEnrollResponse200):
             raise UnknownError("enroll player in activity  returns unknown response")
-
         team = response.team
         return map_team(team)
+    async def leave_player_by_activity(self, activity_id: int, player_id: CoreID) -> None:
+        response = await post_core_activity_id_leave.asyncio(
+            client=self.client, id=activity_id, body=ActivityLeavePlayerRequest(id=player_id)
+        )
+        if isinstance(response, PostCoreActivityIdLeaveResponse404):
+            raise InvalidParameters(f"leave player by activity returns 404 response: {response.message}")
 
 
 class CoreActivityAdminAdapter(ActivityAdminAdapter):
