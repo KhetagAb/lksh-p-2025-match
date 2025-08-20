26b5996a550850b455f41e73cfdd196add5ee15e
Егор Шурыгин <shurygin.egor2009@gmail.com>
2025-08-20 09:39:22 +0300

    Bff tg bot (#149)
    Co-authored-by: Khetag Dzestelov <31622023+KhetagAb@users.noreply.github.com>
    Co-authored-by: Khetag Dzestelov <dzkhetag@gmail.com>

--- bff/lkshmatch/main.py
+++ bff/lkshmatch/main.py
@@ -1,17 +1,19 @@
+import asyncio
+import os
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
 import uvicorn
 import uvloop
-import os
 from fastapi import FastAPI
 
-from lkshmatch.tg_bot.bot import token, bot, router as bot_router
-from lkshmatch.website.auth.auth import auth_router
-from lkshmatch.website.activities import activities_router
-from lkshmatch.website.auth.login_middleware import LoginWallMiddleware
-from lkshmatch.website.activities_gsheets import table_adapter_router
 from lkshmatch.config import settings
+from lkshmatch.tg_bot.bot import token, bot
+from lkshmatch.website.activities import activities_router
+from lkshmatch.website.activities_gsheets import table_adapter_router
+from lkshmatch.website.auth.auth import auth_router
+from lkshmatch.website.auth.login_middleware import LoginWallMiddleware
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[FastAPI]:
@@ -34,7 +36,6 @@ def start() -> None:
     app.include_router(activities_router)
     app.include_router(auth_router, prefix="/auth")
     app.include_router(table_adapter_router, prefix="/table")
-    app.include_router(bot_router, prefix="/bot")
 
     app.add_middleware(LoginWallMiddleware)
 
@@ -48,6 +49,8 @@ def start() -> None:
         # log_level="debug",
         env_file="../.env",
     )
+
+    asyncio.run(bot.polling(none_stop=True))
 
 
 if __name__ == "__main__":
