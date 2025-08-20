e9910042db2e085ba0b24523af53f55807420fd7
Vadim143 <kokorev.vadim.123456789@gmail.com>
2025-08-20 16:49:57 +0300

    Added enrollDeadline to activity

--- core/internal/application/services/activities/update_activity.go
+++ core/internal/application/services/activities/update_activity.go
@@ -9,7 +9,7 @@ import (
 
 func (s *ActivityService) UpdateActivity(ctx context.Context, activity dto.Activity) (*dto.Activity, error) {
 	infra.Infof(ctx, "Updating activity with id=%d", activity.Activity.ID)
-	resActivity, err := s.activityRepository.UpdateActivity(ctx, activity.Activity.ID, &activity.Activity.Title, &activity.Activity.Description, &activity.Activity.SportSectionID, &activity.Creator.ID, activity.Activity.EnrollDeadline)
+	resActivity, err := s.activityRepository.UpdateActivity(ctx, activity)
 
 	if err != nil {
 		return nil, fmt.Errorf("cannot update activity with id=%d: %w", activity.Activity.ID, err)
